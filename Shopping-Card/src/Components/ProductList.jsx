39e49b5f913612c9c3bf963c227439dcd283bcba
Aishaarain <155973754+Aishaarain@users.noreply.github.com>
2025-06-30 23:48:29 +0500

    Add files via upload

---
+++ Shopping-Card/src/Components/ProductList.jsx
@@ -0,0 +1,47 @@
+import { useDispatch } from 'react-redux';
+import { addToCart } from './CardSlice';
+
+
+  const products = [
+  { id: 1, name: "Laptop", price: 750, amount: 1, img: "./public/images.jpg" },
+  { id: 2, name: "Phone", price: 400, amount: 1, img: "./public/phone.jpg" },
+  { id: 3, name: "Headphones", price: 100, amount: 1, img: "./public/headphone.jpg" },
+  { id: 4, name: "Smartwatch", price: 150, amount: 1, img: "./public/smartwatch.jpg" },
+  { id: 5, name: "Tablet", price: 300, amount: 1, img: "./public/tablet.jpg" },
+  { id: 6, name: "Bluetooth Speaker", price: 80, amount: 1, img: "./public/blutoothspeaker.jpg" },
+  { id: 7, name: "Gaming Console", price: 500, amount: 1, img: "./public/gamingconsole.jpg" },
+  { id: 8, name: "Monitor", price: 200, amount: 1, img: "./public/monitor.jpg" },
+  { id: 9, name: "Keyboard", price: 50, amount: 1, img: "./public/keyboard.jpg" },
+  { id: 10, name: "Speaker", price: 500, amount: 1, img: "./public/speaker.jpg" },
+  { id: 11, name: "Drone", price: 650, amount: 1, img: "./public/drone.jpg" },
+  { id: 12, name: "Camera", price: 700, amount: 1, img: "./public/camera.jpg" },
+  
+];
+
+
+
+const ProductList = () => {
+  const dispatch = useDispatch();
+
+  return (<>
+  <h2>Products</h2>   
+    <div className="product-list">
+
+      {products.map(product => (
+
+        <div key={product.id} className="card">
+
+          <img src={product.img} alt="" />
+          <h3>{product.name}</h3>
+          <p>${product.price}</p> 
+            <button onClick={() => dispatch(addToCart(product))}>Add to Cart</button>
+          
+      
+        </div>
+      ))}
+    </div>
+    </>
+  );
+};
+
+export default ProductList;
